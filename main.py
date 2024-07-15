f3c696e40b71796a6006444dad85c0e1c81dd8e5
amirnak <amirnakgamer@gmail.com>
2024-07-15 23:36:39 +0300

    Add: Validation for id

--- main.py
+++ main.py
@@ -14,27 +14,38 @@ import requests
 import json
 
 
-id = 960
+def check_404(request):
+    if not request["pageProps"]["page"] and request["pageProps"]["responseStatus"] == 404:
+        return True
+    return False
 
-req = requests.get(
-    f"https://internship.vk.company/_next/data/1.5.20/vacancy/{id}.json?id={id}").text
 
-vk_response = json.loads(req)
+def good_request(id):
+    req = requests.get(
+        f"https://internship.vk.company/_next/data/1.5.20/vacancy/{id}.json?id={id}").text
 
-good_response = {
-    "page": {
-        "id": vk_response["pageProps"]["page"]["vacancy"]["id"],
-        "title": vk_response["pageProps"]["page"]["vacancy"]["title"],
-        "direction": vk_response["pageProps"]["page"]["vacancy"]["direction"],
-        "city": vk_response["pageProps"]["page"]["vacancy"]["city"],
-        "workFormat": vk_response["pageProps"]["page"]["vacancy"]["format"],
-        "busyness": vk_response["pageProps"]["page"]["vacancy"]["employment_verbose"],
-        "aboutTasksText": vk_response["pageProps"]["page"]["vacancy"]["landing"]["aboutTasksText"]["items"],
-        "aboutSkillsText": vk_response["pageProps"]["page"]["vacancy"]["landing"]["aboutSkillsText"]["items"],
-        "link": f"https://internship.vk.company/_next/data/1.5.20/vacancy/{id}.json?id={id}",
-        "chapter": "Стажировка"
-    }
-}
+    vk_response = json.loads(req)
+
+    if not check_404(vk_response):
+        good_response = {
+            "page": {
+                "id": vk_response["pageProps"]["page"]["vacancy"]["id"],
+                "title": vk_response["pageProps"]["page"]["vacancy"]["title"],
+                "direction": vk_response["pageProps"]["page"]["vacancy"]["direction"],
+                "city": vk_response["pageProps"]["page"]["vacancy"]["city"],
+                "workFormat": vk_response["pageProps"]["page"]["vacancy"]["format"],
+                "busyness": vk_response["pageProps"]["page"]["vacancy"]["employment_verbose"],
+                "aboutTasksText": vk_response["pageProps"]["page"]["vacancy"]["landing"]["aboutTasksText"]["items"],
+                "aboutSkillsText": vk_response["pageProps"]["page"]["vacancy"]["landing"]["aboutSkillsText"]["items"],
+                "link": f"https://internship.vk.company/_next/data/1.5.20/vacancy/{id}.json?id={id}",
+                "chapter": "Стажировка"
+            }
+        }
+        return good_response
+    return vk_response
+
+
+id = 1
 
 with open("response3.json", "w", encoding="utf-8") as file:
-    json.dump(good_response, file, ensure_ascii=False, indent=4)
+    json.dump(good_request(id), file, ensure_ascii=False, indent=4)
